158bbf034cc36d6e83abf8da4e9092b371f139f2
JoaoVictorRS <joaososteam@gmail.com>
2023-01-30 14:14:54 -0300

    condições

---
+++ aula06/ex_11.js
@@ -0,0 +1,11 @@
+var idade = 30
+
+if (idade < 18){
+    console.log("Menor de idade")
+}else{
+    if(idade >= 18 && idade < 60){ 
+        console.log("Maior de idade / Adulto")
+    }else{
+        console.log("Idoso")
+    }
+}
---
+++ aula06/ex_12.js
@@ -0,0 +1,11 @@
+var agora = new Date()
+var horas = agora.getHours()
+
+if (horas < 12){
+    console.log("Bom dia!")
+}else if (horas <= 18){
+    console.log("Boa tarde!")
+}else{
+    console.log("Boa noite!")
+}
+console.log(`Agora são ${horas} horas`)

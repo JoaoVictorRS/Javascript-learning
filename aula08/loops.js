7fe3337bcb812a7f329f211b9f3e3fdd75c91f7b
JoaoVictorRS <joaososteam@gmail.com>
2023-01-31 15:31:23 -0300

    adicionado loop for

--- aula08/loops.js
+++ aula08/loops.js
@@ -7,9 +7,17 @@ while(i < 6){
 }
 */
 
+/*
 var i = 1
 
 do{
     console.log(`Oi ${i}`)
     i++
 }while(i < 6)
+*/
+
+for(i=1;i<6;i++){
+    console.log(`loop ${i}`)
+}
+
+//MESMA COISA DO C AAAAAAAAAAAAAAAAAAAAAAAAAA
